cc2751d8fc7ee2616fcf676ce58c0f3c765b4527
sakshampv <34101559+sakshampv@users.noreply.github.com>
2017-12-20 18:09:03 +0530

    Update uname.sh

--- uname.sh
+++ uname.sh
@@ -6,7 +6,7 @@ grep "$2" "$1" >> new.txt
 INPUT=new.txt
 OLDIFS=$IFS
 IFS=":"
-[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit -1; }
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 1; }
 while read flname dob ssn tel status pro
 do
 	echo "$status"
